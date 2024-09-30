afab664dd74c0c454e3468d9cc2b4de421742d96
Olivier Patry <olivier.patry@gmail.com>
2024-09-30 17:01:58 +0200

    WIP StoreScreenshotTest

--- tasks-app-android/build.gradle.kts
+++ tasks-app-android/build.gradle.kts
@@ -190,6 +190,7 @@ dependencies {
 
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.test.manifest)
+    androidTestImplementation(libs.androidx.uiautomator)
 }
 
 aboutLibraries {
